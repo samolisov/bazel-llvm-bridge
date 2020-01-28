c811f983e890a103055da4222d285ee4efdc2a0c
Pavel Samolysov <samolisov@gmail.com>
2020-01-28 11:59:19 +0300

    Add an example of using tablegen
    The 'llvm_print_physical_registers' example demonstrates
    how to use 'llvm-tablegen' from a 'BUILD' file as a dependency
    for a 'cc_binary' target. The example just parses a register
    file definition for an imaginary target and prints every
    register classes and the registers the class is composed of.
    
    To run the example, do the following:
    
    $ bazel run --action_env LLVM_INSTALL_PREFIX=<path to llvm> //:llvm_print_physical_registers
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

---
+++ examples/llvm/llvm_print_physical_registers.cc
@@ -0,0 +1,28 @@
+#include "target/custom_register_info.h"
+
+#include "llvm/ADT/Triple.h"
+#include "llvm/Support/raw_os_ostream.h"
+
+using namespace llvm;
+
+int main(int argc, char** argv) {
+    Triple TheTriple("custom-linux");
+    CustomRegisterInfo TRI{TheTriple};
+
+    for (auto rcit = TRI.regclass_begin(), rcite = TRI.regclass_end();
+         rcit != rcite; ++rcit) {
+        errs() << "Register Class #" << (*rcit)->getID() << " "
+               << TRI.getRegClassName(*rcit)
+               << " (" << (*rcit)->getNumRegs() << " regs"
+               << ((*rcit)->isAllocatable() ? ", allocatable": "")
+               << ") " << TRI.getRegSizeInBits(**rcit) << " bits:"
+               << "\n";
+        for (auto&& reg : **rcit) {
+            errs() << TRI.getName(reg)
+                   << (Register::isPhysicalRegister(reg) ?
+                        " (physical)\n" : "\n");
+        }
+    }
+
+    return 0;
+}
---
+++ examples/llvm/target/custom_register_info.h
@@ -0,0 +1,33 @@
+#ifndef LLVM_TARGET_CUSTOMREGISTERINFO_H
+#define LLVM_TARGET_CUSTOMREGISTERINFO_H
+
+#include "llvm/CodeGen/TargetRegisterInfo.h"
+
+#define GET_REGINFO_ENUM
+#define GET_REGINFO_HEADER
+#include "custom_register_info.inc"
+
+namespace llvm {
+
+class Triple;
+
+class CustomRegisterInfo final : public CustomGenRegisterInfo {
+public:
+    explicit CustomRegisterInfo(const Triple &TT);
+
+    // Must be implemented to not be abstract
+    const MCPhysReg *
+    getCalleeSavedRegs(const MachineFunction* MF) const override;
+
+    BitVector getReservedRegs(const MachineFunction &MF) const override;
+
+    void eliminateFrameIndex(MachineBasicBlock::iterator MI,
+                             int SPAdj, unsigned FIOperandNum,
+                             RegScavenger *RS = nullptr) const override;
+
+    Register getFrameRegister(const MachineFunction &MF) const override;
+};
+
+} // End llvm namespace
+
+#endif
