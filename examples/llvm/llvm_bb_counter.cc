c62f01b543d4670d4d16f09be36d283ce459af0a
Pavel Samolysov <samolisov@gmail.com>
2020-05-27 11:59:00 +0300

    Optimize the llvm_bb_counter example
    The following changes have been applied:
     - removing unused includes
     - getting the same style for work with ErrorOr<unique_ptr>
       and Expected<unique_ptr>: (*variable)->do().
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- examples/llvm/llvm_bb_counter.cc
+++ examples/llvm/llvm_bb_counter.cc
@@ -1,19 +1,12 @@
 #include "llvm/Bitcode/BitcodeReader.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Error.h"
-#include "llvm/Support/ErrorOr.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/raw_os_ostream.h"
-#include <iostream>
 
 using namespace llvm;
 
 static cl::opt<std::string> FileName(cl::Positional, cl::desc("Bitcode file"), cl::Required);
 
 int main(int argc, char** argv) {
-    cl::ParseCommandLineOptions(argc, argv, "LLVM Hello World\n");
+    cl::ParseCommandLineOptions(argc, argv, "LLVM BB Counter\n");
     LLVMContext context;
 
     ErrorOr<std::unique_ptr<MemoryBuffer>> mb = MemoryBuffer::getFile(FileName);
@@ -22,7 +15,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    Expected<std::unique_ptr<Module>> m = parseBitcodeFile(mb->get()->getMemBufferRef(), context);
+    Expected<std::unique_ptr<Module>> m = parseBitcodeFile((*mb)->getMemBufferRef(), context);
     if (std::error_code ec = errorToErrorCode(m.takeError())) {
         errs() << "Error reading bitcode: " << ec.message() << "\n";
         return -1;
